14aa1239aac66e81124f403e317524ab2c9f847a
gustavo-r-nunes <gustavo.r.nunes97@gmail.com>
2023-07-12 19:17:27 -0300

    game by id

--- src/main/java/com/devsuperior/dslist/Services/GameService.java
+++ src/main/java/com/devsuperior/dslist/Services/GameService.java
@@ -2,12 +2,15 @@ package com.devsuperior.dslist.Services;
 
 import com.devsuperior.dslist.Entities.Game;
 import com.devsuperior.dslist.Repositories.GameRepository;
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
@@ -15,6 +18,7 @@ public class GameService {
     @Autowired
     private GameRepository repository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
 
         List<Game> result = repository.findAll();
@@ -22,4 +26,10 @@ public class GameService {
         return dto;
 
     }
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Optional<Game> result = repository.findById(id);
+        return new GameDTO(result.get());
+    }
+
 }
