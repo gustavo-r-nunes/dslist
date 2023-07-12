9bda0be35ed08c17cb8814379d3b3933f06fab53
gustavo-r-nunes <gustavo.r.nunes97@gmail.com>
2023-07-12 18:54:52 -0300

    implementando dominio

--- src/main/java/com/devsuperior/dslist/Controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/Controllers/GameController.java
@@ -1,10 +1,8 @@
 package com.devsuperior.dslist.Controllers;
 
-import com.devsuperior.dslist.Entities.Game;
 import com.devsuperior.dslist.Services.GameService;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
